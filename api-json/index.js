a49d3b6fe9a10ed963726386d13c4b2b5561ae1e
Alejo <alejandro110498@hotmail.com>
2024-10-20 21:42:36 -0500

    Configuracion de mongo DB

--- api-json/index.js
+++ api-json/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const { urlencoded, json } = require('express');
 const router = require('./routes/signos.routes.js');
 const cors = require('cors');
-const initdb = require ('./config/db.js');
+const mongoose = require('mongoose');
+require ('dotenv').config();
+
 
 const app = express();
+//const port = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(urlencoded({ extended: true }))
 app.use(json())
@@ -12,8 +16,16 @@ app.use(json())
 app.use(cors())
 app.use('/v1/signos', router);
 
-app.listen(4000, () => {
-    console.log('listening at port 4000');
-})
+mongoose.connect(process.env.MONGODB_URI, {
+    /*useNewUrlParser: true,
+    useUnifiedTopology: true,*/
+  })
+  .then(() => console.log('Conectado a MongoDB Atlas'))
+  .catch(err => console.error('Error de conexión a MongoDB:', err));
 
-initdb ();
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
+
+// Inicializar la base de datos
+//initdb();
