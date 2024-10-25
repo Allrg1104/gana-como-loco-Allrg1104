2e78a1403fcfa81ac0ad1ab2e70c65d152a42872
Alejo <alejandro110498@hotmail.com>
2024-10-24 23:27:40 -0500

    Update index.js

--- api-json/index.js
+++ api-json/index.js
@@ -4,34 +4,29 @@ const router = require('./routes/drivers.js');
 const cors = require('cors');
 require('dotenv').config();
 
-// Permitir solicitudes desde el frontend
+// Permitir solicitudes desde el frontend con opciones de CORS
 const corsOptions = {
   origin: 'https://gana-como-loco-allrg1104-front.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Si necesitas enviar cookies
   optionsSuccessStatus: 200
 };
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://gana-como-loco-allrg1104-front.vercel.app'  // Añade el origen de tu frontend aquí
-}));
+app.use(cors(corsOptions));  // Aplica tus opciones de CORS correctamente
 
 app.use(express.json());
 
-
 app.use('/v1/drivers', router);
 
-MONGODB_URI=mongodb+srv//allrg1104:RRp4xn4bxtzh1EWS@allrg1104.xtqyw.mongodb.net/gana_como_loco?retryWrites=true&w=majority&appName=Allrg1104
-
 mongoose.connect(process.env.MONGODB_URI, {
-  })
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(() => console.log('Conectado a MongoDB Atlas'))
 .catch(err => console.error('Error de conexión a MongoDB:', err));
-
-// Aquí puedes añadir tus rutas y modelos
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
